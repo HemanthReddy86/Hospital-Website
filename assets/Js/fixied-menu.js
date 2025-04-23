9b64abb9c84d539a69ae40d9f51610e4014c58b4
Heamnth <gopuhemanth1986@gmail.com>
2025-04-23 11:08:34 +0530

    Hospital web site

---
+++ assets/Js/fixied-menu.js
@@ -0,0 +1,48 @@
+document.addEventListener("DOMContentLoaded", function(){
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 50) {
+          document.getElementById('navbar_top').classList.add('fixed-top');
+          // add padding top to show content behind navbar
+          navbar_height = document.querySelector('.navbar').offsetHeight;
+          document.body.style.paddingTop = navbar_height + 'px';
+        } else {
+          document.getElementById('navbar_top').classList.remove('fixed-top');
+           // remove padding top from body
+          document.body.style.paddingTop = '0';
+        } 
+    });
+  }); 
+
+      // Get the button
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+function togglePopup() {
+    const popup = document.getElementById("popupForm");
+    popup.classList.toggle("show");
+  }
+
+  // Optional: Close popup if clicked outside
+  document.addEventListener('click', function (e) {
+    const popup = document.getElementById("popupForm");
+    const icon = document.querySelector(".icon");
+    if (!popup.contains(e.target) && !icon.contains(e.target)) {
+      popup.classList.remove("show");
+    }
+  });
